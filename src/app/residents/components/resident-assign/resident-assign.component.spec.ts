fb9c432c6399ff57ac82096f2b5967c52f42354f
amine187 <amin.rhouma@gmail.com>
2021-06-05 12:15:38 +0200

    loaded the all the residents except the assignee resident
    The loader start only when the user click on the select box to avoid the over loading

--- src/app/residents/components/resident-assign/resident-assign.component.spec.ts
+++ src/app/residents/components/resident-assign/resident-assign.component.spec.ts
@@ -1,25 +1,70 @@
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ResidentsActions, residentsStore } from '../../store';
 import { ResidentAssignComponent } from './resident-assign.component';
 
 describe('ResidentAssignComponent', () => {
   let component: ResidentAssignComponent;
   let fixture: ComponentFixture<ResidentAssignComponent>;
+  let residentsActions: ResidentsActions;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResidentAssignComponent ]
-    })
-    .compileComponents();
+      declarations: [ResidentAssignComponent],
+      providers: [
+        { provide: 'ResidentsStore', useValue: residentsStore },
+        ResidentsActions,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ResidentAssignComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    residentsActions = TestBed.inject(ResidentsActions);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load all the residents except the assignee', () => {
+    const selectBox =
+      debugElement.nativeElement.querySelector('#assignQuoteSelect');
+    const spyLoadResidents = spyOn(
+      component,
+      'loadResidents'
+    ).and.callThrough();
+    const storeSpy = spyOn(
+      (<any>component).residentsStore,
+      'dispatch'
+    ).and.callThrough();
+    const dataAPI = [
+      { id: 1, username: 'johnathan.doe', firstname: 'johnathan' },
+      { id: 2, username: 'sebastian.müller', firstname: 'sebastian' },
+      { id: 3, username: 'sara.dom', firstname: 'sara' },
+    ];
+    const expectedResult = [
+      { id: 2, username: 'sebastian.müller', firstname: 'sebastian' },
+      { id: 3, username: 'sara.dom', firstname: 'sara' },
+    ];
+
+    component.assigneeID = 1;
+    (<any>component).residentsStore.dispatch(
+      residentsActions.residentsLoaded(dataAPI)
+    );
+    selectBox.dispatchEvent(new Event('focus'));
+    fixture.detectChanges();
+
+    expect(storeSpy).toHaveBeenCalledWith({ type: null });
+    expect(spyLoadResidents).toHaveBeenCalled();
+    expect(component.residents).toEqual(expectedResult);
+  });
+
 });
--- src/app/residents/components/resident-assign/resident-assign.component.ts
+++ src/app/residents/components/resident-assign/resident-assign.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Unsubscribe } from 'redux';
+import { Resident, ResidentsStore } from 'src/app/core/models';
 
 @Component({
   selector: 'app-resident-assign',
   templateUrl: './resident-assign.component.html',
-  styleUrls: ['./resident-assign.component.css']
+  styleUrls: ['./resident-assign.component.css'],
 })
-export class ResidentAssignComponent implements OnInit {
+export class ResidentAssignComponent implements OnInit, OnDestroy {
+  public assignCtrl: FormControl;
+  public residents: Resident[];
 
-  constructor() { }
+  private unsubscribeStore: Unsubscribe;
 
-  ngOnInit(): void {
+  @Input()
+  assigneeID: number | null;
+
+  constructor(
+    @Inject('ResidentsStore') private residentsStore: ResidentsStore
+  ) {
+    this.assigneeID = null;
+    this.assignCtrl = new FormControl('');
+    this.residents = [];
+    this.unsubscribeStore = () => {};
   }
 
+  ngOnInit(): void {
+    this.unsubscribeStore = this.residentsStore.subscribe(() => {
+      this.residents = this.residentsStore
+        .getState()
+        .filter(({ id }) => id !== this.assigneeID);
+    });
+  }
+
+  loadResidents() {
+    this.residentsStore.dispatch({ type: null });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribeStore();
+  }
 }
