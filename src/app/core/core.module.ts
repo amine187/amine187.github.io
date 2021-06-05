ee84c9778ebf28f2b94e12c4ca975cf3791298bd
amine187 <amin.rhouma@gmail.com>
2021-06-05 07:01:37 +0200

    fixed API service import

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ApiService } from './services';
+import { ApiService } from './services/api.service';
 
 @NgModule({
   declarations: [],
