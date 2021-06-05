37baf8819e2259afb9146f5ff0a39017540a935e
amine187 <amin.rhouma@gmail.com>
2021-06-05 07:59:46 +0200

    Display residents in the datatable

--- src/app/residents/residents.component.spec.ts
+++ src/app/residents/residents.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResidentsComponent } from './residents.component';
@@ -8,9 +9,9 @@ describe('ResidentsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResidentsComponent ]
-    })
-    .compileComponents();
+      declarations: [ResidentsComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
