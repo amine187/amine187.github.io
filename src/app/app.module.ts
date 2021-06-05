832ed2df9b21da416e883c4ad3857ff96ffc8a11
amine187 <amin.rhouma@gmail.com>
2021-06-05 06:20:57 +0200

    generated residents module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResidentsComponent } from './residents/residents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResidentsComponent
   ],
   imports: [
     BrowserModule,
