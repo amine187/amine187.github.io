b0639f3cb44e94bcef483d02e5178f3abbe5cf55
amine187 <amin.rhouma@gmail.com>
2021-06-05 10:21:36 +0200

    dispatched the residentsLoaded action on fetchAll service

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ResidentsActions } from 'src/app/residents/store';
 
 import { residents } from '../mocks';
-import { Resident } from '../models';
+import { Resident, ResidentsStore } from '../models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  constructor(
+    @Inject('ResidentsStore') private residentsStore: ResidentsStore,
+    private residentsActions: ResidentsActions
+  ) {}
+
   fetchAll(): Observable<Resident[]> {
-    return of(residents);
+    return of(residents).pipe(
+      map((data) => {
+        this.residentsStore.dispatch(
+          this.residentsActions.residentsLoaded(data)
+        );
+        return data;
+      })
+    );
   }
 }
