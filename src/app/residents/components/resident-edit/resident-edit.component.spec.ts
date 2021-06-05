7d414ac4c89135669daf1f768fd3ce624044de97
amine187 <amin.rhouma@gmail.com>
2021-06-06 00:24:42 +0200

    displayed the title header

--- src/app/residents/components/resident-edit/resident-edit.component.spec.ts
+++ src/app/residents/components/resident-edit/resident-edit.component.spec.ts
@@ -1,25 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
+import { ResidentsActions, residentsStore } from '../../store';
 import { ResidentEditComponent } from './resident-edit.component';
+import { ApiService } from 'src/app/core/services';
 
 describe('ResidentEditComponent', () => {
   let component: ResidentEditComponent;
   let fixture: ComponentFixture<ResidentEditComponent>;
+  let residentsActions: ResidentsActions;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResidentEditComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [ResidentEditComponent],
+      providers: [
+        { provide: 'ResidentsStore', useValue: residentsStore },
+        ResidentsActions,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ResidentEditComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    residentsActions = TestBed.inject(ResidentsActions);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the right resident according to the id in the active router link', inject(
+    [ApiService, ActivatedRoute],
+    (apiService: ApiService, route: ActivatedRoute) => {
+      const dataAPI = {
+        id: 2,
+        username: 'sebastian.müller',
+        firstname: 'sebastian',
+        surname: 'müller',
+      };
+      const expectedResult = 'Change information of resident Sebastian Müller';
+      const APISpy = spyOn(apiService, 'getById').and.returnValue(of(dataAPI));
+      const cardTitle = debugElement.nativeElement.querySelector('.card-title');
+
+      spyOn(route.snapshot.paramMap, 'get').and.returnValue('2');
+      component.ngOnInit();
+      fixture.detectChanges();
+
+      expect(APISpy).toHaveBeenCalledWith(2);
+      expect(component.resident).toEqual(dataAPI);
+      expect(cardTitle.innerText).toEqual(expectedResult);
+    }
+  ));
 });
--- src/app/residents/components/resident-edit/resident-edit.component.ts
+++ src/app/residents/components/resident-edit/resident-edit.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Resident } from 'src/app/core/models';
+import { ApiService } from 'src/app/core/services';
 
 @Component({
   selector: 'app-resident-edit',
   templateUrl: './resident-edit.component.html',
-  styleUrls: ['./resident-edit.component.css']
+  styleUrls: ['./resident-edit.component.css'],
 })
 export class ResidentEditComponent implements OnInit {
+  residentId: number | null;
+  resident: Resident | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {
+    this.residentId = null;
+    this.resident = undefined;
   }
 
+  ngOnInit(): void {
+    this.residentId = this.getResidentId();
+
+    if (this.residentId)
+      this.apiService
+        .getById(this.residentId)
+        .subscribe((resident) => (this.resident = resident));
+  }
+
+  private getResidentId(): number | null {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    return id ? parseInt(id) : null;
+  }
 }
